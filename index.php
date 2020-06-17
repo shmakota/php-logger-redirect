f1bf84cd65400f29b0f593159ca3186c749c48ff
shmakota <47482311+shmakota@users.noreply.github.com>
2020-06-17 00:36:30 -0500

    Update index.php
    Added time & redirect site to logs.

--- index.php
+++ index.php
@@ -3,25 +3,29 @@
 
 # GET IP;
 $target=$_SERVER["REMOTE_ADDR"];
-# Store Filename In a Variable;
+# STORE IN VAR;
 $document="logs.txt";
 
 # OPEN FILE;
 $openDoc=fopen($document, "a");
+
+# GET REDIRECT LINK;
+$link = $_GET['link'];
 
 # WRITE IP & DATE;
 fwrite($openDoc, $target);
 fwrite($openDoc, "\n");
 fwrite($openDoc, date("Y/m/d"));
 fwrite($openDoc, "\n");
+fwrite($openDoc, date("h:i:s A"));
+fwrite($openDoc, "\n");
+fwrite($openDoc, $link);
+fwrite($openDoc, "\n");
 fwrite($openDoc, date("-----------"));
 fwrite($openDoc, "\n");
 
 # CLOSE FILE;
 fclose($openDoc);
-
-# GET REDIRECT LINK;
-$link = $_GET['link'];
 
 # REDIRECT;
 header("Location: $link");
